b3fd0c844ea6088dcc7f40dccbebce9eb8bc380a
YodGuo <YodGuo@users.noreply.github.com>
2026-04-14 02:21:39 +0000

    chore: cleanup for minimal deployable source

--- dist/client/_astro/shared-utils.M_AFdSl3.js
+++
@@ -1,1 +0,0 @@
-globalThis.process??={};globalThis.process.env??={};function l(e){return e.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;")}export{l as e};
--- dist/server/chunks/aggregate_C5aFahLf.mjs
+++
@@ -1,9 +0,0 @@
-globalThis.process ??= {};
-globalThis.process.env ??= {};
-import { s as sql } from "./index_BdvyDh_N.mjs";
-function count(expression) {
-  return sql`count(${sql.raw("*")})`.mapWith(Number);
-}
-export {
-  count as c
-};
